b9a554c24feecdfd2c2275a10fbe8fdd361775dc
soujoud-bia <soujoudbia@gmail.com>
2026-02-21 23:16:29 +0100

    Update BookRepository.kt

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
@@ -23,11 +23,16 @@ class BookRepository {
      * )
      */
     private val booksList = listOf(
-        Book(isbn = "", title = "Clean Code", nbPages = 0),
-        Book(isbn = "", title = "The Pragmatic Programmer", nbPages = 0),
-        Book(isbn = "", title = "Design Patterns", nbPages = 0),
-        Book(isbn = "", title = "Refactoring", nbPages = 0),
-        Book(isbn = "", title = "Head First Design Patterns", nbPages = 0)
+        Book(isbn = "978-9-17-699426-9", title = "Clean Code", nbPages = 120),
+        Book(isbn = "978-2-32-201722-5", title = "The Pragmatic Programmer", nbPages = 540),
+        Book(isbn = "978-0-59-600712-6", title = "Design Patterns", nbPages = 638),
+        Book(isbn = "978-0-59-600874-1", title = "Refactoring", nbPages = 220),
+        Book(isbn = "978-0-59-600712-6", title = "Head First Design Patterns", nbPages = 638),
+        Book(isbn = "978-0-13-276409-4", title = "Code Complete", nbPages = 496),
+        Book(isbn = "979-8-59-236528-7", title = "Head First Java", nbPages = 286),
+        Book(isbn = "978-1-68-392622-1", title = "The Mythical Man-Month", nbPages = 572),
+        Book(isbn = "978-0-13-277804-6", title = "Effective Java", nbPages = 384),
+        Book(isbn = "978-1-49-190526-5", title = "You Don’t Know JS Yet", nbPages = 278),
     )
 
     /**
